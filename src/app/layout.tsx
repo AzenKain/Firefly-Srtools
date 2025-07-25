c1295401a894fe839308323e61f8debce060f523
AzenKain <doduykhanh344@gmail.com>
2025-07-25 09:50:28 +0700

    UPDATE: Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,29 @@ export const metadata: Metadata = {
     shortcut: "/ff-srtool.ico",
     apple: "/ff-srtool.png",
   },
+  openGraph: {
+    title: "Firefly SrTools",
+    description: "SrTools by Kain",
+    url: "https://srtools.kain.id.vn",
+    siteName: "Firefly SrTools",
+    images: [
+      {
+        url: "/ff-srtool.png",
+        width: 1200,
+        height: 630,
+        alt: "Firefly SrTools Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Firefly SrTools",
+    description: "SrTools by Kain",
+    images: ["/ff-srtool.png"],
+  },
 };
-
 
 export default async function RootLayout({
   children,
